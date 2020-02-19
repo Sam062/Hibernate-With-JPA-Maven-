104b0ffe04d536e6a700f7ed7148476e3219c9c7
SAM <sam062>
2020-02-19 19:52:13 +0530

    Added Session Operation #2 Update()..

---
+++ SessionOperation_Update/src/main/java/base/Test.java
@@ -0,0 +1,35 @@
+package base;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		Session ses=null;
+		Transaction tx=null;
+		
+		try {
+			ses=HibernateUtil.getSf().openSession();
+			tx=ses.beginTransaction();
+			
+			ModelClass mc=new ModelClass();
+			mc.setStdId(101);
+			mc.setStdName("XYZ");
+			mc.setStdFee(125.99);
+			
+			ses.update(mc);
+			
+			tx.commit();
+			System.out.println("done");
+		} catch (Exception e) {
+			tx.rollback();
+			e.printStackTrace();
+		}
+		finally {
+			if(ses!=null)
+				ses.close();
+		}
+	}
+
+}
